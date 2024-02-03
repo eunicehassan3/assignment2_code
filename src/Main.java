34586c285e8067e1534a8836e1d88981f8712a03
eunicehassan3 <euniceh.info@gmail.com>
2024-02-03 11:18:34 -0600

    selection sort

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         BubbleSort b = new BubbleSort();
         InsertionSort i = new InsertionSort();
+        SelectionSort s = new SelectionSort();
         int[] arr = {18, 2, 9, 34, -6, 8};
-       // i.insertionSort(arr);
-        //b.bubbleSortI(arr);
         System.out.println(Arrays.toString(b.bubbleSort(arr)));
         System.out.println(Arrays.toString(i.insertionSort(arr)));
+        System.out.println(Arrays.toString(s.selectionSort(arr)));
     }
 }
---
+++ src/SelectionSort.java
@@ -0,0 +1,19 @@
+public class SelectionSort {
+    public static int[] selectionSort(int[] arr){
+        int temp;
+        for(int i = 0; i < arr.length - 1; i++){
+            int minIndex = i;
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+            temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+
+        }
+        return arr;
+    }
+
+}
